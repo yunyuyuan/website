35e94c4c7c71a2b1ea716633cbae336676fca260
yunyuyuan <1607439239@qq.com>
2020-10-19 16:38:59 +0800

    test

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const port = process.env.PORT || 3000
+var os = require('os')
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('操作系统:'+os.type());
 });
 
 server.listen(port,() => {
-  console.log(`computer operator: `);
+  console.log(`start!`);
 });
